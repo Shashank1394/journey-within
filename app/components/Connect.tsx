6dbcbaec6e3035ad181cc64b91a3a49830c0ba63
Shashank Jadhav <shashankjadhav13940@gmail.com>
2026-07-24 23:13:54 +0530

    Refactor code structure for improved readability and maintainability

--- app/components/Connect.tsx
+++ app/components/Connect.tsx
@@ -3,55 +3,58 @@
 import Image from "next/image";
 
 const Connect = () => {
-  return (
-    <section className="connect-section">
-      <div className="container">
-        <div className="row align-items-stretch">
-          {/* Columns 2-7 */}
-          <div className="col-6 offset-1 d-flex connect-copy-col">
-            <div className="connect w-100 d-flex flex-column justify-content-center text-center">
-              <h2 className="title">Hey, I&apos;m Riccardo</h2>
+	return (
+		<section className="connect-section">
+			<div className="container">
+				<div className="row align-items-stretch">
+					{/* Columns 2-7 */}
+					<div className="col-6 offset-1 d-flex connect-copy-col">
+						<div className="connect w-100 d-flex flex-column justify-content-center text-center">
+							<h2 className="title">Hey, I&apos;m Riccardo</h2>
 
-              <p className="intro">
-                And I&apos;m looking forward to guiding you through our
-                beautiful journey, both in India and within. I invite you to
-                discover what pushed me to create this project
-              </p>
-            </div>
-          </div>
+							<div className="connect-svg">
+								<Image
+									src="/assets/connect.svg"
+									alt="And I'm looking forward to guiding you through our beautiful journey, both in India and within. I invite you to discover what pushed me to create this project"
+									width={600}
+									height={200}
+								/>
+							</div>
+						</div>
+					</div>
 
-          {/* Columns 8-11 */}
-          <div className="col-5 col-md-3 offset-md-1 d-flex align-items-center justify-content-center">
-            <div className="photo">
-              <Image
-                src="/assets/riccardo.webp"
-                alt="Riccardo"
-                width={450}
-                height={450}
-                priority
-              />
-            </div>
-          </div>
+					{/* Columns 8-11 */}
+					<div className="col-5 col-md-3 offset-md-1 d-flex align-items-center justify-content-center">
+						<div className="photo">
+							<Image
+								src="/assets/riccardo.webp"
+								alt="Riccardo"
+								width={450}
+								height={450}
+								priority
+							/>
+						</div>
+					</div>
 
-          {/* Column 8 */}
-          <div className="col-2 offset-6 offset-md-7 d-flex justify-content-center">
-            <Image
-              src="/assets/arrow.svg"
-              alt="Arrow"
-              width={300}
-              height={150}
-              className="arrow"
-            />
-          </div>
+					{/* Column 8 */}
+					<div className="col-2 offset-6 offset-md-7 d-flex justify-content-center">
+						<Image
+							src="/assets/arrow.svg"
+							alt="Arrow"
+							width={300}
+							height={150}
+							className="arrow"
+						/>
+					</div>
 
-          {/* Columns 9-10 */}
-          <div className="col-5 offset-6 col-md-5 offset-md-7 d-flex justify-content-center text-center cta-container">
-            <p className="cta">Let&apos;s connect!</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+					{/* Columns 9-10 */}
+					<div className="col-5 offset-6 col-md-5 offset-md-7 d-flex justify-content-center text-center cta-container">
+						<p className="cta">Let&apos;s connect!</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Connect;
