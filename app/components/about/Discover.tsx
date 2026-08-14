f286c0b1865e2408dadc9c1c31a6176bac6041b6
Shashank Jadhav <shashankjadhav13940@gmail.com>
2026-08-15 00:57:07 +0530

    feat(discover): add Instagram icon and enhance layout with new styles

--- app/components/about/Discover.tsx
+++ app/components/about/Discover.tsx
@@ -3,58 +3,65 @@
 import Image from "next/image";
 
 const Discover = () => {
-  return (
-    <section className="discover-section">
-      <div className="container">
-        <div className="row align-items-stretch">
-          {/* Columns 2-7 */}
-          <div className="col-7 d-flex">
-            <div className="discover w-100 d-flex flex-column justify-content-center text-center">
-              <h2 className="title">Discover your future</h2>
+	return (
+		<section className="discover-section">
+			<div className="container">
+				<div className="row align-items-stretch">
+					{/* Columns 2-7 */}
+					<div className="col-7 d-flex">
+						<div className="discover w-100 d-flex flex-column justify-content-center text-center">
+							<h2 className="title">Discover your future</h2>
 
-              <div className="discover-svg">
-                <Image
-                  src="/assets/about/abt-comp.svg"
-                  alt="These trips have been thought and designed for this moment of your life. A purpose-giving experience is what is needed to boost you into your next phase."
-                  width={600}
-                  height={200}
-                />
-              </div>
-            </div>
-          </div>
+							<div className="discover-svg">
+								<Image
+									src="/assets/about/abt-comp.svg"
+									alt="These trips have been thought and designed for this moment of your life. A purpose-giving experience is what is needed to boost you into your next phase."
+									width={600}
+									height={200}
+								/>
+								<Image
+									src="/assets/about/Instagram_icon.png"
+									alt="Follow us on Instagram"
+									width={80}
+									height={80}
+									className="discover-instagram-icon"
+								/>
+							</div>
+						</div>
+					</div>
 
-          {/* Columns 8-11 */}
-          <div className="col-4 col-md-3 offset-md-1 d-flex align-items-center justify-content-center">
-            <div className="photo">
-              <Image
-                src="/assets/discover.png"
-                alt="Riccardo"
-                width={450}
-                height={450}
-                priority
-              />
-            </div>
-          </div>
+					{/* Columns 8-11 */}
+					<div className="col-4 col-md-3 offset-md-1 d-flex align-items-center justify-content-center">
+						<div className="photo">
+							<Image
+								src="/assets/discover.png"
+								alt="Riccardo"
+								width={450}
+								height={450}
+								priority
+							/>
+						</div>
+					</div>
 
-          {/* Column 8 */}
-          <div className="col-2 offset-7 offset-md-7 d-flex justify-content-center">
-            <Image
-              src="/assets/arrow.svg"
-              alt="Arrow"
-              width={300}
-              height={150}
-              className="arrow"
-            />
-          </div>
+					{/* Column 8 */}
+					<div className="col-2 offset-7 offset-md-7 d-flex justify-content-center">
+						<Image
+							src="/assets/arrow.svg"
+							alt="Arrow"
+							width={300}
+							height={150}
+							className="arrow"
+						/>
+					</div>
 
-          {/* Columns 9-10 */}
-          <div className="col-9 offset-3 col-md-7 offset-md-6 d-flex justify-content-center text-center">
-            <p className="cta">Journey Within</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+					{/* Columns 9-10 */}
+					<div className="col-9 offset-3 col-md-7 offset-md-6 d-flex justify-content-center text-center">
+						<p className="cta">Journey Within</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Discover;
