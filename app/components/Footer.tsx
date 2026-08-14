02a88ae405ced2a0b0477c5469e2aa0f9fdfda9b
Shashank Jadhav <shashankjadhav13940@gmail.com>
2026-08-15 00:17:28 +0530

    feat(footer): add stamps image and update footer layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,24 +1,33 @@
 import Image from "next/image";
 
 const Footer = () => {
-  return (
-    <footer className="footer-component">
-      <div className="container-fluid px-0">
-        <div className="row g-0">
-          <div className="col-12">
-            <Image
-              src="/assets/footer.webp"
-              alt="footer image"
-              width={1383}
-              height={391}
-              sizes="100vw"
-              className="footer-image"
-            />
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="footer-component">
+			<div className="container-fluid px-0">
+				<div className="row g-0">
+					<div className="col-12">
+						<div className="footer-image-wrapper">
+							<Image
+								src="/assets/footer.webp"
+								alt="footer image"
+								width={1383}
+								height={391}
+								sizes="100vw"
+								className="footer-image"
+							/>
+							<Image
+								src="/assets/stamps.png"
+								alt="Journey Within stamps"
+								width={531}
+								height={485}
+								className="footer-stamps"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
